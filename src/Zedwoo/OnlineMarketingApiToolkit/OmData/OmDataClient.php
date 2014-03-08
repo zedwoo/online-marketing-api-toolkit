9539573c430241d22de6f7a4215ba55d605a7e87
Michael Janssen <office@zedwoo.de>
2014-03-08 13:56:52 +0100

    Added MagicMethods and Annotations to OmData

--- src/Zedwoo/OnlineMarketingApiToolkit/OmData/OmDataClient.php
+++ src/Zedwoo/OnlineMarketingApiToolkit/OmData/OmDataClient.php
@@ -4,6 +4,7 @@ namespace Zedwoo\OnlineMarketingApiToolkit\OmData;
 use Guzzle\Common\Collection;
 use Guzzle\Service\Client;
 use Guzzle\Service\Description\ServiceDescription;
+use Guzzle\Service\Resource\Model;
 use Guzzle\Service\Builder\ServiceBuilder;
 use Guzzle\Common\Exception\InvalidArgumentException;
 
@@ -11,6 +12,23 @@ use Guzzle\Common\Exception\InvalidArgumentException;
 /**
  * Class OmDataClient
  * @package Zedwoo\OnlineMarketingApiToolkit\OmData
+ *
+ * @method  model accountStatus () {@command OmData accountStatus}
+ * @method  model serpsWeeklyAddKeyword (array $args = array()) {@command OmData serpsWeeklyAddKeyword}
+ * @method  model serpsWeeklyDeleteKeyword (array $args = array()) {@command OmData serpsWeeklyDeleteKeyword}
+ * @method  model serpsWeeklyGetKeywords (array $args = array()) {@command OmData serpsWeeklyGetKeywords}
+ * @method  model serpsWeeklyGetRankings (array $args = array()) {@command OmData serpsWeeklyGetRankings}
+ * @method  model serpsDailyAddKeyword (array $args = array()) {@command OmData serpsDailyAddKeyword}
+ * @method  model serpsDailyDeleteKeyword (array $args = array()) {@command OmData serpsDailyDeleteKeyword}
+ * @method  model serpsDailyGetKeywords (array $args = array()) {@command OmData serpsDailyGetKeywords}
+ * @method  model serpsDailyGetRankings (array $args = array()) {@command OmData serpsDailyGetRankings}
+ * @method  model serpsDirectGetRankings (array $args = array()) {@command OmData serpsDirectGetRankings}
+ * @method  model urlsPagerank (array $args = array()) {@command OmData urlsPagerank}
+ * @method  model urlsServer (array $args = array()) {@command OmData urlsServer}
+ * @method  model urlsUrlCheck (array $args = array()) {@command OmData urlsUrlCheck}
+ * @method  model keywordsDifficulty (array $args = array()) {@command OmData keywordsDifficulty}
+ * @method  model keywordsUserIntent (array $args = array()) {@command OmData keywordsUserIntent}
+ * @method  model statusGet () {@command OmData statusGet}
  */
 class OmDataClient extends Client
 {
